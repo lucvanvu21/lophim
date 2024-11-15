98920b95a17d0a7f4f8b00f1602c535c953e566d
VVluc <dtc21h4802010511@ictu.edu.vn>
2024-11-15 15:49:54 +0700

    fix seo

--- src/app/(guest)/[type]/page.tsx
+++ src/app/(guest)/[type]/page.tsx
@@ -3,27 +3,54 @@ import { moviesRequestApi } from '@/requestApi/movies/movies';
 import { Typography } from '@mui/material';
 import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'Xem Phim Lẻ Phim Bộ  Phim Mới Nhất ',
-  description:
-    'lophim.site - Phim Đang Chiếu Phim Hay Phim Mới Nhất chất lượng cao miễn phí với phụ đề tiếng việt, có nhiều thể loại phim phong phú, đặc sắc, nhiều bộ phim hay nhất - mới nhất.',
+export const generateMetadata = async ({
+  params,
+  searchParams,
+}: {
+  params: { type: string };
+  searchParams: { page: number };
+}) => {
+  // Bạn có thể tùy chỉnh title và description động dựa vào tham số loại phim và trang
+  const page = searchParams.page || 1;
+  let genre = '';
+  let description = '';
+
+  if (params.type === 'tv') {
+    genre = 'phim lẻ';
+    description = 'Xem phim lẻ với chất lượng cao và đầy đủ thể loại phim lẻ mới nhất';
+  } else if (params.type === 'movies') {
+    genre = 'phim bộ';
+    description = 'Xem phim bộ hay nhất, phim bộ mới nhất miễn phí';
+  } else if (params.type === 'hoat-hinh') {
+    genre = 'hoạt hình';
+    description = 'Xem phim hoạt hình hấp dẫn, đầy màu sắc với phụ đề tiếng Việt';
+  }
+
+  return {
+    title: `Xem ${genre} - Lophim.site - Phim ${genre} Mới Nhất Chất Lượng Cao`,
+    description: description || 'Lophim.site - Phim đang chiếu, phim hay, phim mới nhất chất lượng cao miễn phí.',
+  };
 };
+
 const Movies = async ({ params, searchParams }: { params: { type: string }; searchParams: { page: number } }) => {
   const page = searchParams.page || 1;
   let res: any;
-  if (params.type === 'phim-le') {
+  if (params.type === 'tv') {
     res = await moviesRequestApi.getAllMoviesForUser('phim-le', page, 18);
-  } else if (params.type === 'phim-bo') {
+  } else if (params.type === 'movies') {
     res = await moviesRequestApi.getAllMoviesForUser('phim-bo', page, 18);
   } else if (params.type === 'hoat-hinh') {
     res = await moviesRequestApi.getAllMoviesForUser('hoat-hinh', page, 18);
   }
+
+  // console.log('---->res---genres:', res);
   return (
     <>
       <Typography variant="h4" fontWeight={600} component={'h1'}>
-        Danh sách phim {params.type === 'phim-le' ? 'lẻ' : params.type === 'phim-bo' ? 'bộ' : 'hoạt hình'}
+        Danh sách phim{' '}
+        {params.type === 'movies' ? 'lẻ' : params.type === 'tv' ? 'bộ' : params.type === 'hoat-hinh' ? 'hoạt hình' : ''}
       </Typography>
-      <MoviesList movies={res.data?.items} paginate={res?.data?.params?.pagination} />
+      <MoviesList movies={res?.data?.items} paginate={res?.data?.params?.pagination} />
     </>
   );
 };
