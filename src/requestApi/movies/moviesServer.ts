a5c8e43c31a4f778a8aee76456d92387398fdc47
VVluc <dtc21h4802010511@ictu.edu.vn>
2025-01-29 09:11:20 +0700

    fix cache

--- src/requestApi/movies/moviesServer.ts
+++ src/requestApi/movies/moviesServer.ts
@@ -2,15 +2,15 @@ import { http } from '@/utils/actions';
 
 export const moviesServer = {
   //user
-  getMoviesByType: async <T>(category: string, slug: string, page: number =1, limit: number = 20, search?: string) => {
+  getMoviesByType: async <T>(category: string, slug: string, page: number = 1, limit: number = 20, search?: string) => {
     const slug1 = slug === undefined ? '' : `/${slug}`;
     return await http.get<T>(`/films/${category}${slug1}?page=${page}&limit=${limit}&keyword=${search}`, {
-      next: { revalidate: 1600 },
+      // next: { revalidate: 1600 },
+      cache: 'no-store',
     });
   },
   getMovieBySlug: async <T>(slug: string) => {
-    return await http.get<T>(`/film/${slug}`, { next: { revalidate: 2600 } });
+    return await http.get<T>(`/film/${slug}`, { cache: 'no-store' });
   },
   //admin
-  
 };
